7955faad784e5c54bd494c6c312dc5917e194d27
hatada <hatada.koki@jp.fujitsu.com>
2017-11-09 11:57:39 +0900

    ビルド

--- build/index.js
+++ build/index.js
@@ -1,30 +1,27 @@
+"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var bodyParser = require("body-parser");
-var googlehome = require('google-home-notifier');
 var superagent = require("superagent");
-googlehome.device('Google Home'); // Change to your Google Home name
-// or if you know your Google Home IP
-// googlehome.ip('192.168.x.x');
-googlehome.accent('ja'); // optional: 'us'= american voice (default), 'uk'= british voice
-googlehome.notify("こんにちは", function (res) {
-    console.log(res);
-});
+var HttpsProxyAgent = require("https-proxy-agent");
+var proxy = process.env.http_proxy;
+var agent = new HttpsProxyAgent(proxy);
+var port = 3000;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", function (req, res) {
     res.status(200);
-    res.json({ speech: "aaa" });
+    res.json({ speech: "zatsu is running. port=" + port });
     res.end();
 });
 var context = "";
 function kaiwa(text) {
     return new Promise(function (resolve, reject) {
-        // 50486f713564674a4a514c2f34726d2e4378374a6a726276326c7a526678414b4a682e7272775934676e44
-        var token = "50486f713564674a4a514c2f34726d2e4378374a6a726276326c7a526678414b4a682e7272775934676e44";
+        var token = process.env.DOCOMO_TOKEN;
         var url = "https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=" + token;
         var post = superagent.post(url);
+        post.agent(agent);
         post.send({
             "utt": text,
             "context": context,
@@ -70,14 +67,4 @@ app.post("/api/dialog", function (req, res) {
         res.end();
     }
 });
-app.post("/api/speech", function (req, res) {
-    console.log(req.body);
-    res.status(200);
-    res.json({});
-    res.end();
-    var text = req.body.text;
-    googlehome.notify(text, function (res) {
-        console.log(res);
-    });
-});
-app.listen(3000);
+app.listen(port);
